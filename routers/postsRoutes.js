9efa1820452b9f514090f89f5842a283d3be0ee5
b <121223458+H4RmYxDD@users.noreply.github.com>
2025-10-15 11:37:04 +0200

    mitortenik 😭😭

--- routers/postsRoutes.js
+++ routers/postsRoutes.js
@@ -1,5 +1,8 @@
 import express from "express";
 import * as Post from "../data/post.js";
+import * as User from "../data/user.js";
+import bcrypt from "bcrypt";
+import jwt from "jsonwebtoken";
 
 const router = express.Router();
 
@@ -29,10 +32,39 @@ router.put("/:id", (req, res) => {
   const post = Post.getPostById(id);
   if (!post) return res.status(404).json({ error: "Post not found" });
 
-  Post.updatePost(id, userId || post.userId, title || post.title, content || post.content);
+  Post.updatePost(
+    id,
+    userId || post.userId,
+    title || post.title,
+    content || post.content
+  );
   res.json({ message: "Post updated" });
 });
 
+router.post("/login", (req, res) => {
+  const { email, password } = req.body;
+  if (!email || !password) {
+    res.status(400).json({ message: "invalid credentials" });
+  }
+  const user = User.getUserByEmail(email);
+  if (!user) {
+    return res.status(404).json({ message: "invalid credentials" });
+  }
+  // if (!bcrypt.compareSync(password, user.password))
+  //   return res.status(400).json({ message: "invalid creds" });
+  const token = jwt.sign({ id: user.id, email: user.email }, "secret_key", {
+    expiresIn: "30s",
+  });
+  res.json({ token: token });
+});
+
+router.get("/my", (req, res) => {});
+
+function auth(req, res, next) {
+  const accessToken = req.headers.authorize;
+  if (!accessToken) return res.status(401).json({ message: "unathorized" });
+  const token = jwt.verify(accessToken, "secret_key");
+}
 
 router.delete("/:id", (req, res) => {
   const id = req.params.id;
